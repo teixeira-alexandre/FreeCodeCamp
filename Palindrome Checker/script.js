69f4218a9d1bf9a125e88bd8c3a75e3f3302b04b
teixeira-alexandre <alexandre1997teixeira@gmail.com>
2024-04-20 11:47:57 -0300

    complete palindrome project

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -2,17 +2,19 @@ const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const resultText = document.getElementById("result");
 const inputStr = textInput.value.split();checkButton.addEventListener("click", () => {
+    const cleanStr = textInput.value.replace(/[^A-Za-z0-9]/g, '').toLowerCase().split("").join("");
+    const reversedStr = textInput.value.replace(/[^A-Za-z0-9]/g, '').toLowerCase().split("").reverse().join("");
 
     if (textInput.value === "") {
         alert("Please input a value!");
+        return
     } else if (textInput.value.length === 1) {
         resultText.textContent = `${textInput.value} is a palindrome!`
-    } else if (textInput.value.replace(/_\W/g, '').split("").join("") === textInput.value.replace(/_\W/g, '').split("").reverse().join("")) {
-        resultText.textContent = `${textInput.value} is a palindrome!`
+    } else if (cleanStr === reversedStr) {
+        resultText.textContent = `${textInput.value} is a palindrome!`;
     } else {
         resultText.textContent = `${textInput.value} is not a palindrome`
     }
-    
-
-
 });
+
+
